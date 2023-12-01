12d91fde965a8d146c71e81d43b20110ebe966d9
les <adkajsdasduoisaudkksak@dsfalasjkdlajsdk.ru>
2023-12-01 18:25:45 +0300

    Add func1 to Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev"); }
+
+    public static void func1() {
+        System.out.print("Это добавлено в ветке dev func 1"); }
 }
